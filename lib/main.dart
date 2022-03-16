8ba3d4a5e08bdf70292a36b53f911ff9d5db825f
AlchemistDark <justplus@rambler.ru>
2022-03-16 22:18:12 +0700

    Some renames.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';                                     // Стандартная библиотека виджетов.
 import 'simple_presentator.dart';                                           // Класс для асинхронной работы со списком строк.
+import 'string_widget.dart';                                                // Класс, где хранится виджет строки списка.
 
 void main() {
   final DataSource dataSource = DataSource();
@@ -30,27 +31,23 @@ class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title, required this.dataSource}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState(title, dataSource);
+  _MyHomePageState createState() => _MyHomePageState(dataSource);
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   final DataSource dataSource;
-  final String title;
   int _ind = -1;                                     // Поле, которое хранит индекс строки списка, которая в данный момент редактируется.
                                                      // -1 означает, что в данный момент таких строк нет.
   late SimplePresentator present;                    // Данное поле это объект потока данных. Класс описан на строке 4.
-  _MyHomePageState(this.title, this.dataSource){
+  _MyHomePageState(this.dataSource){
     present = SimplePresentator(dataSource);
   }
 
   void _dataAdd(String str) async {                            // Создаёт строку списка.
     await present.create(str);                                 // Метод класса из строки 4 для создания строки списка.
   }
-  void _dataChecked(int stringIndex) async {                   // Помечает строку как редактируемую.
-    _ind = stringIndex;
-    print("Start $_ind $stringIndex");
-    await present.loadAll();                                   // Это событие нужно чтобы обновить экран. Это метод класса на строке 4.
-    print("End $_ind $stringIndex");
+  void _dataChecked(int stringIndex) {                         // Помечает строку как редактируемую.
+    setState(() {_ind = stringIndex;});                        // setState обновляет экран.
   }
   void _dataEdit(String oldStr, String newStr) async {         // Редактирует строку.
     await present.edit(oldStr, newStr);                        // Метод класса из строки 4 для редактирования строки списка.
@@ -60,6 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
     await present.delete(str);                                 // Метод класса из строки 4 для удаления строки списка.
   }
 
+  // Конструктор.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,26 +68,24 @@ class _MyHomePageState extends State<MyHomePage> {
             StreamBuilder<List<String>>(
               initialData: [],
               stream: present.sData,
-              builder: (context, snapShot) {                   // Здесь должно быть много кода по превращению потока в виджет...
+              builder: (context, snapShot) {                  // Здесь должно быть много кода по превращению потока в виджет...
                 List<String> lst = snapShot.data!;
                 ListView lV = ListView.builder(
-                  itemCount: lst.length,                       // Эта строка сообщает ListView.builder сколько всего элементов в списке.
+                  itemCount: lst.length,                      // Эта строка сообщает ListView.builder сколько всего элементов в списке.
                   itemBuilder: (BuildContext context, int index) {
-                    print("index $_ind $index");
-                    if (_ind == index) {
-                      return MyTextField(
-                        str: lst[index],
-                        callBackEdit: (text) {_dataEdit(lst[index], text);},     // Строка ~45. Праметр text должен как-то импортироваться из виджета, который сам в другом классе.
-                      );
-                    }
-                    return MyRow(
+                    String _str = lst[index];
+                    print('строка $_str, $index, $_ind');
+                    bool _isEditMode = _ind == index;
+                    return StringWidget(                                       // Виджет описан в классе на строке 5.ь
+                      isEditMode: _isEditMode,
                       str: lst[index],
-                      callBackChecked: () {_dataChecked(index);},        // Строка ~40.
-                      callBackDelete: () {_dataDelete(lst[index]);},           // Строка ~50.
+                      onSelected: () {_dataChecked(index);},                   // Строка ~40. callBack.
+                      onEdited: (text) {_dataEdit(lst[index], text);},         // Строка ~45. callBack.
+                                                                               // Праметр text должен как-то импортироваться из виджета, который сам в другом классе.
+                      onDeleted: () {_dataDelete(lst[index]);},                // Строка ~50. callBack.
                     );
                   }
                 );
-                print(lV);
                 return lV;
               },
             )
@@ -109,54 +105,10 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ]
       ),
-      appBar: AppBar(title: Text(title)) // Заголовок окна.
+      appBar: AppBar(title: const Text('Стрингошлёпалка3')) // Заголовок окна.
     );
   }
 }
 
-class MyTextField extends StatelessWidget {
-  final String str;                               // Сюда передаётся та строка списка, ради которой и замутили виджет.
-  final callBackEdit;                             // Сюда передаётся функция редактирования строки.
-
-  const MyTextField({Key? key, required this.str, required this.callBackEdit}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      onSubmitted: callBackEdit,
-      decoration: InputDecoration(border: OutlineInputBorder(), hintText: str)
-    );
-  }
-}
-
-class MyRow extends StatelessWidget {
-  final String str;                               // Сюда передаётся та строка списка, ради которой и замутили виджет.
-  final callBackChecked;                          // Сюда передаётся функция, помечающая строку как редактируемую.
-  final callBackDelete;                           // Сюда передаётся функция удаления строки.
-
-  const MyRow({Key? key,  required this.str, required this.callBackChecked, required this.callBackDelete}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      textDirection: TextDirection.ltr,
-      children: [
-        ElevatedButton.icon(
-          onPressed: callBackChecked,
-          style: ElevatedButton.styleFrom(primary: Colors.green, fixedSize: Size(5, 5)),
-          icon: Icon(Icons.edit),
-          label: Text("")
-        ),
-        ElevatedButton.icon(
-          onPressed: callBackDelete,
-          style: ElevatedButton.styleFrom(primary: Colors.red, fixedSize: Size(20, 20)),
-          icon: Icon(Icons.remove),
-          label: Text("")
-        ),
-        Text(str),
-      ],
-    );
-  }
-}
 
 
--- lib/string_widget.dart
+++ lib/string_widget.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
 class StringWidget extends StatelessWidget {
-  final bool isEditable;                          // Сюда передаётся редактируемая это строка или нет.
+  final bool isEditMode;                          // Сюда передаётся редактируемая это строка или нет.
   final String str;                               // Сюда передаётся та строка списка, ради которой и замутили виджет.
-  final void Function() callBackChecked;          // Сюда передаётся функция, помечающая строку как редактируемую.
-  final void Function(String text) callBackEdit;  // Сюда передаётся функция редактирования строки.
-  final void Function() callBackDelete;           // Сюда передаётся функция удаления строки.
+  final void Function() onSelected;          // Сюда передаётся функция, помечающая строку как редактируемую.
+  final void Function(String text) onEdited;  // Сюда передаётся функция редактирования строки.
+  final void Function() onDeleted;           // Сюда передаётся функция удаления строки.
 
   const StringWidget(
-    {Key? key, required this.isEditable, required this.str, required this.callBackChecked, required this.callBackEdit, required this.callBackDelete}
+    {Key? key, required this.isEditMode, required this.str, required this.onSelected, required this.onEdited, required this.onDeleted}
   ) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (isEditable) {
+    if (isEditMode) {
       return
         TextField(
-          onSubmitted: callBackEdit,
+          onSubmitted: onEdited,
           decoration: InputDecoration(border: OutlineInputBorder(), hintText: str)
         );
     }
@@ -24,13 +24,13 @@ class StringWidget extends StatelessWidget {
       textDirection: TextDirection.ltr,
       children: [
         ElevatedButton.icon(
-          onPressed: callBackChecked,
+          onPressed: onSelected,
           style: ElevatedButton.styleFrom(primary: Colors.green, fixedSize: Size(5, 5)),
           icon: Icon(Icons.edit),
           label: Text("")
         ),
         ElevatedButton.icon(
-          onPressed: callBackDelete,
+          onPressed: onDeleted,
           style: ElevatedButton.styleFrom(primary: Colors.red, fixedSize: Size(20, 20)),
           icon: Icon(Icons.remove),
           label: Text("")
